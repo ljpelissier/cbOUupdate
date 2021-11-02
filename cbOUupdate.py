b9534dcb75f8c50ec9274fbf226a4c5a79e90ef8
ljpelissier <l.j.pelissier+github@gmail.com>
2021-11-02 15:34:37 -0400

    first commit early

---
+++ cbOUupdate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+ author: Louis Pelssier
+ 
+ 
+"""
+import configparser
+import os
+import pandas as pd
+import subprocess
+
+#year offset based on grade level
+YEAROFFSET = 9  #9 for 8th grade 13 for High School
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+print(config)
