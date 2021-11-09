d61bc39dc456137745b8c358bded5b59a65cc8b2
ljpelissier <l.j.pelissier+github@gmail.com>
2021-11-09 11:31:31 -0500

    updated .sh

---
+++ cbOUupdate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#python cbOUupdate.py
+export GAMCFGDIR=/home/administrator/GAMConfig
+/home/administrator/bin/gamadv-xtd3/gam print cros limit_to_ou /Chromebooks/StudentChromebooks/Forrestdale/ClassOf2022 serialnumber todrive tdfileid 1xl25IxjvmZb7ZzfKpMiJfZq18tFM3aPRue7XK9ZfhdI tdtitle 'cbOUupdate' tdsheet 'ClassOf2022' tdupdatesheet 
+/home/administrator/bin/gamadv-xtd3/gam print cros limit_to_ou /Chromebooks/StudentChromebooks/Forrestdale/ClassOf2023 serialnumber todrive tdfileid 1xl25IxjvmZb7ZzfKpMiJfZq18tFM3aPRue7XK9ZfhdI tdtitle 'cbOUupdate' tdsheet 'ClassOf2023' tdupdatesheet
+/home/administrator/bin/gamadv-xtd3/gam print cros limit_to_ou /Chromebooks/StudentChromebooks/Forrestdale/ClassOf2024 serialnumber todrive tdfileid 1xl25IxjvmZb7ZzfKpMiJfZq18tFM3aPRue7XK9ZfhdI tdtitle 'cbOUupdate' tdsheet 'ClassOf2024' tdupdatesheet
+/home/administrator/bin/gamadv-xtd3/gam print cros limit_to_ou /Chromebooks/StudentChromebooks/Forrestdale/ClassOf2025 serialnumber todrive tdfileid 1xl25IxjvmZb7ZzfKpMiJfZq18tFM3aPRue7XK9ZfhdI tdtitle 'cbOUupdate' tdsheet 'ClassOf2025' tdupdatesheet
+/home/administrator/bin/gamadv-xtd3/gam print cros limit_to_ou /Chromebooks/StudentChromebooks/Forrestdale/ClassOf2026 serialnumber todrive tdfileid 1xl25IxjvmZb7ZzfKpMiJfZq18tFM3aPRue7XK9ZfhdI tdtitle 'cbOUupdate' tdsheet 'ClassOf2026' tdupdatesheet
+/home/administrator/bin/gamadv-xtd3/gam print cros limit_to_ou /Chromebooks/StudentChromebooks/Forrestdale/ClassOf2027 serialnumber todrive tdfileid 1xl25IxjvmZb7ZzfKpMiJfZq18tFM3aPRue7XK9ZfhdI tdtitle 'cbOUupdate' tdsheet 'ClassOf2027' tdupdatesheet
+/home/administrator/bin/gamadv-xtd3/gam print cros limit_to_ou /Chromebooks/StudentChromebooks/Forrestdale/ClassOf2028 serialnumber todrive tdfileid 1xl25IxjvmZb7ZzfKpMiJfZq18tFM3aPRue7XK9ZfhdI tdtitle 'cbOUupdate' tdsheet 'ClassOf2028' tdupdatesheet
+/home/administrator/bin/gamadv-xtd3/gam print cros limit_to_ou /Chromebooks/StudentChromebooks/Forrestdale/ClassOf2029 serialnumber todrive tdfileid 1xl25IxjvmZb7ZzfKpMiJfZq18tFM3aPRue7XK9ZfhdI tdtitle 'cbOUupdate' tdsheet 'ClassOf2029' tdupdatesheet
+/home/administrator/bin/gamadv-xtd3/gam print cros limit_to_ou /Chromebooks/StudentChromebooks/Forrestdale/ClassOf2030 serialnumber todrive tdfileid 1xl25IxjvmZb7ZzfKpMiJfZq18tFM3aPRue7XK9ZfhdI tdtitle 'cbOUupdate' tdsheet 'ClassOf2030' tdupdatesheet
